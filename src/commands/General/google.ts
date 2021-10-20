654d10f408cb98d7620568b39161f2c42675d2a6
Pratyush <86536877+pratyush4932@users.noreply.github.com>
2021-10-20 21:00:19 +0530

    Update google.ts

--- src/commands/General/google.ts
+++ src/commands/General/google.ts
@@ -1,98 +1,41 @@
 import MessageHandler from '../../Handlers/MessageHandler'
-
 import BaseCommand from '../../lib/BaseCommand'
-
 import WAClient from '../../lib/WAClient'
-
 import { IParsedArgs, ISimplifiedMessage } from '../../typings'
-
 import axios from 'axios'
 
 export default class Command extends BaseCommand {
-
     constructor(client: WAClient, handler: MessageHandler) {
-
         super(client, handler, {
-
             command: 'google',
-
             aliases: ['g', 'search'],
-
             description: 'Search on the web ',
-
-            category: 'general',
-
-            dm: true,
-
-            usage: `${client.config.prefix}google [query]`
-
+            category: 'media',
+            usage: `${client.config.prefix}google [query]`,
+            baseXp: 10
         })
-
     }
-
-    // static count = 0
-
     run = async (M: ISimplifiedMessage, { joined }: IParsedArgs): Promise<void> => {
-
-        // Adds mod check
-
-        // if (!this.client.config.mods?.includes(M.sender.jid)) return void null
-
-        if (!this.client.config.gkey) return void null
-
+        if (!this.client.config.gkey) return void M.reply('No google API key set')
         if (!joined) return void M.reply('🔎 Provide a search term')
-
-        // if (Command.count > 75) return void M.reply('🔎 Search limit reached')
-
-        // Command.count += 1
-
         const term = joined.trim()
-
         await axios
-
             .get(
-
                 `https://www.googleapis.com/customsearch/v1?q=${term}&key=${this.client.config.gkey}&cx=baf9bdb0c631236e5`
-
             )
-
             .then((res) => {
-
-                // console.log(res);
-
                 if (res.status !== 200) return void M.reply(`🔍 Error: ${res.status}`)
-
                 let result = ``
-
                 let index = 1
-
                 for (const item of res.data?.items) {
-
                     result += `*🍥${index}.Title* : ${item.title}\n*🔗Link* : ${item.link}\n*📖Snippet* : ${item.snippet}\n\n`
-
                     index++
-
                 }
-
                 // return void M.reply(`🔍Command Used : ${Command.count} times\n Result for *${term}*\n\n\n ${result}`)
-
-                return void this.client.sendMessage(M.from,`🔍 Result for *${term}*\n\n\n ${result}`,{quoted:M.WAMessage,contextInfo: {
-    externalAdReply: {
-           title:`Google search-${term}`,
-          body: `searched by Pratyush`,
-         thumbnailUrl: `https://64.media.tumblr.com/1beaebc66d9be0d12231cb7e886855ad/e05ed8993097dd72-a6/s500x750/734cb50014b3280edc8bdcb15f42322edd55b067.jpg`,
-        
-    }
-}})
-
+                return void M.reply(`🔍 Result for *${term}*\n\n\n ${result}`)
             })
-
             .catch((err) => {
-
                 M.reply(`🔍 Error: ${err}`)
-
             })
-
     }
-
 }
