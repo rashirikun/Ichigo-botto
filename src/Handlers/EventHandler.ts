e2a2e0ef75e905ea2722fef56318845fb0051c20
Pratyush <86536877+pratyush4932@users.noreply.github.com>
2021-10-22 02:06:25 +0530

    Update EventHandler.ts

--- src/Handlers/EventHandler.ts
+++ src/Handlers/EventHandler.ts
@@ -1,4 +1,4 @@
-import { MessageType, WAParticipantAction,Mimetype } from '@adiwajshing/baileys'
+import { MessageType, WAParticipantAction } from '@adiwajshing/baileys'
 import chalk from 'chalk'
 import request from '../lib/request'
 import WAClient from '../lib/WAClient'
@@ -16,15 +16,14 @@ export default class EventHandler {
         const data = await this.client.getGroupData(event.jid)
         if (!data.events) return void null
         const add = event.action === 'add'
-       const tt= `*@${event.participants[0].split('@')[0]}* has left the chat 👋`
         const text = add
-            ? `*Welcome to*- ${group.subject || '___'} -\n\n💠 *Group Description:*\n${
+            ? `*Welcome to* - ${group.subject || '___'} -\n\n💠 *Group Description:*\n${
                   group.desc
               }\n\nHope you follow the rules and have fun!\n*‣ ${event.participants
                   .map((jid) => `@${jid.split('@')[0]}`)
                   .join(', ')}*`
             : event.action === 'remove'
-            ? tt
+            ? `*@${event.participants[0].split('@')[0]}* bye👋,we will never miss you`
             : `*@${event.participants[0].split('@')[0]}* got ${this.client.util.capitalize(event.action)}d${
                   event.actor ? ` by *@${event.actor.split('@')[0]}*` : ''
               }`
@@ -32,21 +31,16 @@ export default class EventHandler {
             mentionedJid: event.actor ? [...event.participants, event.actor] : event.participants
         }
         if (add) {
-             const n = [
-                'https://c.tenor.com/06u-XgIGVGcAAAPo/welcome-anime.mp4'
-            ]
-            let image = n[Math.floor(Math.random() * n.length)]
+            let image = (await this.client.getProfilePicture(event.jid)) || this.client.assets.get('404.png')
+            if (typeof image === 'string') image = await request.buffer(image)
             if (image)
-                return void (await this.client.sendMessage(event.jid, image, MessageType.video, {
-                     mimetype: Mimetype.gif,
+                return void (await this.client.sendMessage(event.jid, image, MessageType.image, {
                     caption: text,
                     contextInfo
                 }))
         }
-           return void this.client.sendMessage(event.jid, text, MessageType.extendedText, { contextInfo })
+        return void this.client.sendMessage(event.jid, text, MessageType.extendedText, { contextInfo })
     }
-    
-    
 }
 
 interface IEvent {
