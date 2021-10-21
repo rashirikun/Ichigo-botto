e955fbdeed825f8c48e96b1ebe4f6e4ce09f5bab
Pratyush <86536877+pratyush4932@users.noreply.github.com>
2021-10-21 13:33:21 +0530

    Update Disable.ts

--- src/commands/Dev/Disable.ts
+++ src/commands/Dev/Disable.ts
@@ -7,18 +7,29 @@ export default class Command extends BaseCommand {
     constructor(client: WAClient, handler: MessageHandler) {
         super(client, handler, {
             command: 'disable',
-            description: 'Disables the given command from being used',
-            category: 'dev',
+            description: 'Disables the given command from being used globally',
+            category: 'config',
             dm: true,
-            usage: `${client.config.prefix}disable [command] | (reason)`
+            usage: `${client.config.prefix}config [command] | (reason)`,
+            modsOnly: true,
+            baseXp: 0
         })
     }
 
     run = async (M: ISimplifiedMessage, { joined }: IParsedArgs): Promise<void> => {
-        if (!this.client.config.mods?.includes(M.sender.jid)) return void null
         const split = joined.split('|')
         const key = split[0].toLowerCase().trim()
         if (!key) return void (await M.reply(`Provide the command you want to disable`))
+        const feature = key === 'chatbot' ? key : ''
+        if (feature) {
+            const data = await this.client.getFeatures(feature)
+            if (!data.state) return void M.reply(`🟨 *${this.client.util.capitalize(feature)}* is already *inactive*`)
+            await this.client.DB.feature.updateOne({ feature: feature }, { $set: { ['state']: false } }).catch(() => {
+                return void M.reply(`🟨 *${this.client.util.capitalize(feature)}* could not be disabled`)
+            })
+            this.client.features.set('chatbot', false)
+            return void M.reply(`🟩 *${this.client.util.capitalize(feature)}* is now inactive`)
+        }
         const command = this.handler.commands.get(key) || this.handler.aliases.get(key)
         if (!command) return void (await M.reply(`No command found`))
         if (await this.client.DB.disabledcommands.findOne({ command: command.config.command }))
