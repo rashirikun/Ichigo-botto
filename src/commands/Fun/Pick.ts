5b929cecf59b23efe3d2f17e19eb57c0e4f1d34a
Pratyush <86536877+pratyush4932@users.noreply.github.com>
2021-10-24 03:05:19 +0530

    Create Pick.ts

---
+++ src/commands/Fun/Pick.ts
@@ -0,0 +1,27 @@
+
+import MessageHandler from '../../Handlers/MessageHandler'
+import BaseCommand from '../../lib/BaseCommand'
+import WAClient from '../../lib/WAClient'
+import { ISimplifiedMessage } from '../../typings'
+
+export default class Command extends BaseCommand {
+    constructor(client: WAClient, handler: MessageHandler) {
+        super(client, handler, {
+            command: 'pick',
+            description: 'Tags all users in group chat',
+            aliases: ['tag'],
+            category: 'general',
+            usage: `${client.config.prefix}everyone`,
+            baseXp: 20
+        })
+    }
+
+    run = async (M: ISimplifiedMessage): Promise<void> => {
+       
+     const mem=M.groupMetadata?.participants?.map(User) => user.jid
+     let lol=mem[Math.floor(Math.random() * mem.length)]
+     return void(await M.reply(lol) .catch((reason: any) => M.reply(`an error occurred, Reason: ${reason}`)))
+    
+   
+}
+}
