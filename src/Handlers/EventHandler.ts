40c020fce7ed0a5048f1d0b7ba238610ed268e88
Pratyush <86536877+pratyush4932@users.noreply.github.com>
2021-10-24 18:48:44 +0530

    Update EventHandler.ts

--- src/Handlers/EventHandler.ts
+++ src/Handlers/EventHandler.ts
@@ -42,7 +42,7 @@ export default class EventHandler {
         }
       
    
-        return void this.client.sendMessage(event.jid, text, MessageType.extendedtext,{ contextInfo })
+        return void this.client.sendMessage(event.jid, text, MessageType.extendedText,{ contextInfo })
     
 
 }
