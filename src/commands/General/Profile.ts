1bd665fabda200df7aaa2432269eebffa0a28b53
Pratyush <86536877+pratyush4932@users.noreply.github.com>
2021-10-24 10:10:23 +0530

    Update Profile.ts

--- src/commands/General/Profile.ts
+++ src/commands/General/Profile.ts
@@ -34,36 +34,36 @@ export default class Command extends BaseCommand {
                 'https://i.pinimg.com/736x/ca/e7/8a/cae78ad7f8e6459ad20bde350e2eb78b.jpg'
         }
         const data = await this.client.getUser(user)
-        if (data.Xp < 50) {
+        if (data.Xp <= 50) {
             var role = '🎯️ Noobie'
     
-        } else if (data.Xp>50) {
+        } else if (data.Xp>50 && data.Xp<=150) {
             var role = '✨️ Elite'
     
-        } else if (data.Xp > 150){
+        } else if (data.Xp > 150 && data.Xp<=250){
             var role = '🔶️ Ace'
     
-        } else if (data.Xp > 250) {
+        } else if (data.Xp > 250 && data.Xp<=350) {
             var role = '💎️ Supreme' 
     
 
-} else if (data.Xp > 350) {
+} else if (data.Xp > 350 && data.Xp<=450) {
             var role = '🛡️ Legendary' 
     
-} else if (data.Xp > 450) {
+} else if (data.Xp > 450 && data.Xp<=550) {
             var role = '🛡️ Legendary II' 
     
-    } else if (data.Xp > 550) {
+    } else if (data.Xp > 550 && data.Xp<=650) {
             var role = '🛡️ Legendary III' 
     
-} else if (data.Xp > 650) {
+} else if (data.Xp > 650 && data.Xp<=750) {
             var role = '❄️ Mystic' 
     
-} else if (data.Xp > 750) {
+} else if (data.Xp > 750 && data.Xp<=850) {
             var role = '❄️ Mystic II' 
     
-} else if (data.Xp > 850) {
-            var role = '❄️ Mystic III' 
+} else if (data.Xp > 850 && data.Xp<=950) {
+        var role = '❄️ Mystic III' 
    
 
 } else { 
@@ -80,7 +80,7 @@ export default class Command extends BaseCommand {
             undefined,
             `🏮 *Username: ${username}*\n\n🎗 *About: ${
                 (await this.client.getStatus(user)).status || 'None'
-            }*\n\n⭐ *XP: ${data.Xp || 0}*\n\n🔺️Rank:${role}\n\n👑 *Admin: ${
+            }*\n\n⭐ *XP: ${data.Xp || 0}*\n\n🔺️ *Rank:${role}*\n\n👑 *Admin: ${
                 M.groupMetadata?.admins?.includes(user) || false
             }*\n\n✖ *Ban ${data.ban || false}*`
         )
